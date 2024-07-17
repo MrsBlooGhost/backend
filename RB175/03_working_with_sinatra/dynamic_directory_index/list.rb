553f531c1886dc1c25e1f98da7ef458714397c2f
Diana <dngo199@gmail.com>
2024-07-17 14:57:17 -0400

    Starting users and interests

---
+++ RB175/03_working_with_sinatra/dynamic_directory_index/list.rb
@@ -0,0 +1,12 @@
+# list.rb
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+
+get "/" do
+  @files = Dir.glob("public/*").map { |file| File.basename(file) }.sort
+  @files.reverse! if params[:sort] == "descending"
+
+  erb :list
+end
